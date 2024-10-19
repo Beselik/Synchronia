b64f421fd74ea96341a605d4ae1923013968431a
curlypriscilla <lantp4@mail.broward.edu>
2024-10-19 02:54:26 -0400

    Added more functionality to upload interface
    Added more JS functionality to upload interface

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-const dropArea = document.getElementById 
+// file upload interface 
+const dropArea = document.getElementById('drop-area');
+const fileInput = document.getElementById('file-input');
+
+// function to prevent default browser behavior 
+function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropegation();
+}
+
+dropArea.addEventListener('dragover', preventDefaults);
+dropArea.addEventListener('dragenter', preventDefaults);
+dropArea.addEventListener('dragleave', preventDefaults);
+
+dropArea.addEventListener('drop', handleDrop);
+
+dropArea.addEventListener('dragover', () => {
+    dropArea.classList.add('drag-over');
+  });
+  
+  dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('drag-over');
+  });
+
